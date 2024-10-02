739ba8344cd50def8afb24787a4e07060696a18c
Mohamed Hassan <mohamedhassanwork2887@gmail.com>
2024-10-02 11:53:35 +0300

    Step 4 & 5

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 projectData = {};
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(express.static("website"));
 
 app.get("/getData", (req, res) => {
--- website/app.js
+++ website/app.js
@@ -1,9 +1,10 @@
 /* Global Variables */
 // Baseurl + My API Key
 const baseURL = "https://api.openweathermap.org/data/2.5/weather";
-const apiKey = ;
+const apiKey = "";
+const postURL = "http://localhost:3000/addData";
 
-// Async function to fetch weather data based on zip code..
+/* Async function to fetch weather data based on zip code. */
 // I first defined that the function is asynchronous with async.
 // I told the program to wait for the fetch result but also add await to tells the code to wait for the response from the API before moving on to the next line since it can take a awhile.
 async function getWeatherData(zip) {
@@ -21,23 +22,51 @@ async function getWeatherData(zip) {
   return data; // Return the fetched data
 }
 
+/* Second POST function to store data in database */
+async function postWeatherData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("Unable to post data");
+  }
+
+  return response.json(); // Return the JSON response
+}
+
 // Event listener for the generate button
 document.getElementById("generate").addEventListener("click", async () => {
   const zipCode = document.getElementById("zip").value;
   const feelings = document.getElementById("feelings").value;
-  // try and catch for catching errors while fetching weather data
+
   try {
     const weatherData = await getWeatherData(zipCode);
     const temperature = weatherData.main.temp;
     const date = new Date().toLocaleDateString();
 
-    //   updating the DOM
+    // Create data object to POST
+    const postData = {
+      temperature,
+      date,
+      userResponse: feelings,
+    };
 
+    // posts the weather data along with user feelings to the server
+    await postWeatherData(postURL, postData);
+
+    // Update DOM UI
     document.getElementById("date").innerText = `Date: ${date}`;
-    // prettier-ignore
-    document.getElementById("temp").innerText = `Temperature: ${temperature} °C`;
+    document.getElementById(
+      "temp"
+    ).innerText = `Temperature: ${temperature} °C`;
     document.getElementById("content").innerText = `Feelings: ${feelings}`;
   } catch (error) {
-    console.error("Error fetching weather data:", error);
+    console.error("Error:", error);
   }
 });
