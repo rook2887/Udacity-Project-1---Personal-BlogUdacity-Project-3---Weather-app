bfbcccf2866381b721db8e1c449292a5c7b57e26
Mohamed Hassan <mohamedhassanwork2887@gmail.com>
2024-10-02 09:09:03 +0300

    POST

--- server.js
+++ server.js
@@ -5,15 +5,26 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware (optional, for JSON parsing)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("website"));
 
-// Define a route
-app.get("/", (req, res) => {
+app.get("/getData", (req, res) => {
   res.send("Hello, Express!");
+});
+
+app.post("/addData", (req, res) => {
+  const { temperature, date, userResponse } = req.body; 
+
+  // Add the received data to projectData
+  projectData = {
+    temperature: temperature,
+    date: date,
+    userResponse: userResponse,
+  };
+
+  res.send({ message: "Data added successfully", data: projectData });
 });
 
 // Start the server
